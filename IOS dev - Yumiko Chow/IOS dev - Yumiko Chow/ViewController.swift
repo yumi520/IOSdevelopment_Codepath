ee9536251b2fd95550efb3b1919adf76afe5adb4
Yumiko (Yumi) Chow <75456756+yumi520@users.noreply.github.com>
2023-06-16 13:07:14 -0400

    added prework to repository

---
+++ IOS dev - Yumiko Chow/IOS dev - Yumiko Chow/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  IOS dev - Yumiko Chow
+//
+//  Created by Yumiko Chow on 6/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        
+        func changeColor() -> UIColor{
+
+                let red = CGFloat.random(in: 0...1)
+                let green = CGFloat.random(in: 0...1)
+                let blue = CGFloat.random(in: 0...1)
+
+                return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            
+            }
+    }
+    
+}
+
